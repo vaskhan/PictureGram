e771c264707b7f498ee181ffd4a281720cde0d76
Vasiliy Khanin <ya-v.khanin@yandex.ru>
2025-01-08 22:07:17 +0300

    Внесены правки по комментариям ревьюера.

--- PictureGram/ProfileViewController/ProfileViewController.swift
+++ PictureGram/ProfileViewController/ProfileViewController.swift
@@ -9,83 +9,78 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    private let profileImageView: UIImageView = {
+        let image = UIImage(named: "Avatar")
+        let view = UIImageView(image: image)
+        return view
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.font = UIFont(name: "SFPro-Bold", size: 23)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_nov"
+        label.font = UIFont(name: "SFPro-Regular", size: 13)
+        label.textColor = .lightGray
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.font = UIFont(name: "SFPro-Regular", size: 13)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let exitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "exitButtonImage")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addProfileImage()
-        addNameLabel()
-        addLoginLabel()
-        addDescriptionLabel()
-        addButtonExit()
+        setupViews()
+        setupConstraints()
     }
     
-    private func addProfileImage() {
-        let profileImage = UIImage(named: "Avatar")
-        let profileImageView = UIImageView(image: profileImage)
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(profileImageView)
-        
+    private func setupViews() {
+        [profileImageView, nameLabel, loginLabel, descriptionLabel, exitButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
         NSLayoutConstraint.activate([
             profileImageView.heightAnchor.constraint(equalToConstant: 70),
             profileImageView.widthAnchor.constraint(equalToConstant: 70),
             profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 76)
-        ])
-    }
-    
-    private func addNameLabel() {
-        let nameLabel = UILabel()
-        nameLabel.text = "Екатерина Новикова"
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.font = UIFont(name: "SFPro-Bold", size: 23)
-        nameLabel.textColor = .white
-        view.addSubview(nameLabel)
-        
-        NSLayoutConstraint.activate([
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            
             nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 154)
-        ])
-    }
-    
-    private func addLoginLabel() {
-        let loginLabel = UILabel()
-        loginLabel.text = "@ekaterina_nov"
-        loginLabel.translatesAutoresizingMaskIntoConstraints = false
-        loginLabel.font = UIFont(name: "SFPro", size: 13)
-        loginLabel.textColor = .ypLightGray
-        view.addSubview(loginLabel)
-        
-        NSLayoutConstraint.activate([
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            
             loginLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            loginLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 180)
-        ])
-    }
-    
-    private func addDescriptionLabel() {
-        let descriptionLabel = UILabel()
-        descriptionLabel.text = "Hello, world!"
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.font = UIFont(name: "SFPro", size: 13)
-        descriptionLabel.textColor = .white
-        view.addSubview(descriptionLabel)
-        
-        NSLayoutConstraint.activate([
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
             descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            descriptionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 206)
-        ])
-    }
-    
-    private func addButtonExit() {
-        let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "exitButtonImage")?.withRenderingMode(.alwaysOriginal), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(button)
-        
-        NSLayoutConstraint.activate([
-            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
-            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 99),
-            button.heightAnchor.constraint(equalToConstant: 24),
-            button.widthAnchor.constraint(equalToConstant: 24)
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            
+            exitButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            exitButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 55),
+            exitButton.heightAnchor.constraint(equalToConstant: 24),
+            exitButton.widthAnchor.constraint(equalToConstant: 24)
         ])
     }
 }
+
+
